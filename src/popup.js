8352c59b3a10b61bfa9fd788a622460ba666f362
糖饼 <1987.tangbin@gmail.com>
2015-12-14 11:30:40 +0800

    透明浮层指令

---
+++ src/popup.js
@@ -0,0 +1,11 @@
+/* global require */
+
+'use strict';
+
+require('./css/ui-popup.css');
+
+var directives = require('./directives');
+
+directives.popup('popup', {
+    template: '<div class="ui-popup"><div ng-transclude></div></div>'
+});
