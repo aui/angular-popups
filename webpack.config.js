e7044bdf690e346d94d62619503de01fb6d2a562
糖饼 <1987.tangbin@gmail.com>
2015-12-16 15:27:56 +0800

    修改输出的文件名

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,13 @@ var version = require('./package.json').version;
 
 module.exports = {
     entry: {
-        popups: './src/index.js'
+        'angular-popups': './src/index.js'
     },
     output: {
         path: 'dist',
         filename: '[name].js'
     },
+    devtool: 'source-map',
     plugins: [
         new ExtractTextPlugin('[name].css'),
         new webpack.BannerPlugin('popups@' + version + ' | https://github.com/aui/angular-popups')
@@ -19,10 +20,35 @@ module.exports = {
         angular: 'angular'
     },
     module: {
+        preLoaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'jshint-loader'
+            }
+        ],
         loaders: [
             //{test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader')}, // 把 CSS 抽离成独立的文件
-            {test: /\.css$/, loader: 'style!css'},
-            {test: /\.md$/, loader: 'html!markdown'}
+            {test: /\.css$/, loader: 'style!css?minimize'}
         ]
+    },
+
+    // more options in the optional jshint object
+    jshint: {
+        // any jshint option http://www.jshint.com/docs/options/
+        // i. e.
+        camelcase: true,
+
+        // jshint errors are displayed by default as warnings
+        // set emitErrors to true to display them as errors
+        emitErrors: false,
+
+        // jshint to not interrupt the compilation
+        // if you want any file with jshint errors to fail
+        // set failOnHint to true
+        failOnHint: false,
+
+        // custom reporter function
+        reporter: function(errors) { }
     }
 };
